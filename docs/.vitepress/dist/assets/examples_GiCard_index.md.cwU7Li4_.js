42e62efae47e724a788fa7d24dde8e93158e1cca
黄林 <huanglin@jxtech.net>
2025-05-27 17:54:54 +0800

    build

---
+++ docs/.vitepress/dist/assets/examples_GiCard_index.md.cwU7Li4_.js
@@ -0,0 +1,10 @@
+import{aw as s,y as n,z as e,a_ as t}from"./chunks/framework.BJA90vUj.js";const h=JSON.parse('{"title":"GiCard 按钮","description":"","frontmatter":{},"headers":[],"relativePath":"examples/GiCard/index.md","filePath":"examples/GiCard/index.md"}'),p={name:"examples/GiCard/index.md"};function l(i,a,r,d,c,o){return e(),n("div",null,a[0]||(a[0]=[t(`<h1 id="gicard-按钮" tabindex="-1">GiCard 按钮 <a class="header-anchor" href="#gicard-按钮" aria-label="Permalink to &quot;GiCard 按钮&quot;">​</a></h1><h2 id="基础用法" tabindex="-1">基础用法 <a class="header-anchor" href="#基础用法" aria-label="Permalink to &quot;基础用法&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>&lt;template&gt;</span></span>
+<span class="line"><span>  &lt;div&gt;</span></span>
+<span class="line"><span>  &lt;/div&gt;</span></span>
+<span class="line"><span>&lt;/template&gt;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>&lt;script lang=&quot;ts&quot; setup&gt;</span></span>
+<span class="line"><span>&lt;/script&gt;</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>&lt;style&gt;</span></span>
+<span class="line"><span>&lt;/style&gt;</span></span></code></pre></div>`,3)]))}const g=s(p,[["render",l]]);export{h as __pageData,g as default};
