760107e7ad060427e7455b5bb43c76236bd1346d
木糖醇 <53759423+lin-97@users.noreply.github.com>
2025-09-11 09:16:16 +0800

    feat: 文档通知

--- packages/docs/.vitepress/config.ts
+++ packages/docs/.vitepress/config.ts
@@ -21,62 +21,6 @@ export default defineConfig({
       // { text: '内置CSS类名', link: '/style' },
       // { text: `v${packageJson.version}`, link: '' }
     ],
-
-    sidebar: {
-      '/guide/': [
-        {
-          text: '指南',
-          items: [
-            { text: '介绍', link: '/guide/introduction' },
-            { text: '快速开始', link: '/guide/quick-start' },
-            { text: '安装', link: '/guide/installation' },
-            { text: '更新日志', link: '/guide/changelog' }
-          ]
-        }
-      ],
-      '/components/': [
-        {
-          text: '基础组件',
-          items: [
-            { text: 'Button 按钮', link: '/components/button' },
-            { text: 'Card 卡片', link: '/components/card' },
-            { text: 'Dialog 对话框', link: '/components/dialog' },
-            { text: 'Form 表单', link: '/components/form' },
-            { text: 'Table 表格', link: '/components/table' },
-            { text: 'Tabs 标签页', link: '/components/tabs' }
-          ]
-        },
-        {
-          text: '输入组件',
-          items: [
-            { text: 'InputGroup 输入组', link: '/components/input-group' },
-            { text: 'InputSearch 搜索输入', link: '/components/input-search' }
-          ]
-        },
-        {
-          text: '布局组件',
-          items: [
-            { text: 'Grid 栅格', link: '/components/grid' },
-            { text: 'PageLayout 页面布局', link: '/components/page-layout' }
-          ]
-        },
-        {
-          text: '其他组件',
-          items: [
-            { text: 'EditTable 可编辑表格', link: '/components/edit-table' }
-          ]
-        }
-      ],
-      '/tools/': [
-        {
-          text: '工具',
-          items: [
-            { text: 'createSelectDialog', link: '/tools/createSelectDialog' }
-          ]
-        }
-      ]
-    },
-
     socialLinks: [
       { icon: 'github', link: 'https://github.com/lin-97/gi-component' }
     ]
